21585f3fd8f81a79e8d174fdd6a0c873082fc1b2
DendyArizkiKuswardana <113276468+DendyArizkiKuswardana@users.noreply.github.com>
2022-09-12 07:42:06 +0700

    Update Tugas1.sh

--- Tugas1.sh
+++ Tugas1.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo -----------------------------------
-echo ("Tugas 1 SISOP Kelas-A")
-echo -----------------------------------
-echo ("notes: ")
-echo ("Halo dunia!")
-echo ("Ini adalah skrip Bash pertamaku di Linux")
+echo "-----------------------------------"
+echo "Tugas 1 SISOP Kelas-A"
+echo "-----------------------------------"
+echo "Nama : Dendy Arizki Kuswardana"
+echo "NPM  : 21083010006"
+echo "-----------------------------------"
+echo "notes: "
+echo "-----------------------------------"
+echo "Halo dunia!"
+echo "Ini adalah skrip Bash pertamaku di Linux"
